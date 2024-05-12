1af06053233840d5bb15ddc8f1b35f3090fadafb
Nazım Hüseyin Özen <hozen343@gmail.com>
2024-05-12 23:22:23 +0300

    Add project files.

---
+++ Minefield_WindowsForm/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Minefield_WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lnlMine = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(594, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 21);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(576, 384);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // lnlMine
+            // 
+            this.lnlMine.AutoSize = true;
+            this.lnlMine.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lnlMine.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lnlMine.Location = new System.Drawing.Point(737, 189);
+            this.lnlMine.Name = "lnlMine";
+            this.lnlMine.Size = new System.Drawing.Size(0, 39);
+            this.lnlMine.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(694, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(603, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SCORE :";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.ForeColor = System.Drawing.Color.Red;
+            this.lblScore.Location = new System.Drawing.Point(739, 284);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(28, 29);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Image = global::Minefield_WindowsForm.Properties.Resources.Icons8_Ios7_Editing_Rotation_Ccw_48;
+            this.button2.Location = new System.Drawing.Point(668, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 60);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(606, 179);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 60);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(816, 427);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lnlMine);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mine Field";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lnlMine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Minefield_WindowsForm/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minefield_WindowsForm
+{
+
+
+    public partial class Form1 : Form
+    {
+        int mineNumber = 5;
+        int score = 0;
+        public Form1()
+        {
+
+            InitializeComponent();
+            lnlMine.Text = mineNumber.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            int mine1, mine2, mine3, mine4, mine5;
+
+            Random rnd = new Random();
+            mine1 = rnd.Next(1, 12);
+            mine2 = rnd.Next(12, 24);
+            mine3 = rnd.Next(24, 38);
+            mine4 = rnd.Next(38, 46);
+            mine5 = rnd.Next(46, 55);
+
+
+            for (int i = 1; i <= 54; i++)
+            {
+                Button btnTemp = new Button();
+                btnTemp.Name = "btn" + i.ToString();
+                btnTemp.Size = new System.Drawing.Size(40, 40);
+                btnTemp.UseVisualStyleBackColor = true;
+                btnTemp.Tag = null;
+                if (mine1 == i || mine2 == i || mine3 == i || mine4 == i || mine5 == i)
+                {
+                    btnTemp.Tag = true;
+                }
+                else
+                {
+                    btnTemp.Tag = false;
+
+                }
+
+                btnTemp.Click += btnTemp0_Click;
+                flowLayoutPanel1.Controls.Add(btnTemp);
+
+            }
+        }
+
+        private void btnTemp0_Click(object sender, EventArgs e)
+        {
+            
+            Button clickedBtn = (Button)sender;
+            bool mineFound = (bool)clickedBtn.Tag;
+            if (mineFound)
+            {
+                mineNumber--;
+                lnlMine.Text = mineNumber.ToString();
+                clickedBtn.Enabled = false;
+                if (mineNumber == 0)
+                {
+                    clickedBtn.BackColor = Color.Red;
+                    MessageBox.Show("YOU LOSE", "TRY AGAIN", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Thread.Sleep(3000);
+                    Application.Restart();
+
+                }
+
+                clickedBtn.BackColor = Color.Red;
+            }
+            else
+            {
+               
+                score++;
+                lblScore.Text = score.ToString();
+
+                clickedBtn.BackColor = Color.Green;
+                if(score==49)
+                {
+                    MessageBox.Show("YOU WIN", "Congratulations", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    Thread.Sleep(3000);
+                    Application.Restart();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+           
+        }
+    }
+}
+
+
+
